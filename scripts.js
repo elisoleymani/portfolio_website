44bb730c759d128bdc1ae347cf4f69e02bc48be0
elisoleymani <elhamsoleymani019@gmail.com>
2023-10-17 18:14:29 +0200

    Change the div sections for the whole page

---
+++ scripts.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.headlinename')
+ScrollReveal().reveal('.tagline', { delay: 500 })
+ScrollReveal().reveal('.punchline', { delay: 2000 })
+ScrollReveal().reveal('.headline', { duration: 2000 });
+ScrollReveal().reveal('.widget', { interval: 200 });
